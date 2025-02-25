46474871ecefa1a213df470a8eb655d7cc91e474
ramaganjikunta78 <ramaganjikunta78@gmail.com>
2025-02-25 09:25:49 -0500

    BackendAPIcode

--- src/main/java/com/example/employee/EmployeeController.java
+++ src/main/java/com/example/employee/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import com.example.employee.services.EmployeeService;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins="http://localhost:3000")
 
 public class EmployeeController {
 	
--- src/main/java/com/example/employee/entity/EmployeeEntity.java
+++ src/main/java/com/example/employee/entity/EmployeeEntity.java
@@ -1,6 +1,8 @@
 package com.example.employee.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -10,6 +12,7 @@ import lombok.Data;
 @Table(name="employee")
 public class EmployeeEntity {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private String firstname;
 	
